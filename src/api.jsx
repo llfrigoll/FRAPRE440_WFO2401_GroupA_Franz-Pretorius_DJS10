41c4b903391de90693d2f0f4bf547d55a86b6a68
Franz <franzj.pretorius.2003@gmail.com>
2024-06-11 13:21:40 +0200

    Fixed children

--- src/api.jsx
+++ src/api.jsx
@@ -3,37 +3,34 @@ import { useEffect, useState } from "react"
 
 function BlogComponent() {
     const [blogs, setBlogs] = useState([])
-    let isSuccessful = false
+    const [isSuccessful, setIsSuccessful] = useState(false);
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(data => {
-            isSuccessful = true
+            setIsSuccessful(true)
             setBlogs(data)
         })
         .catch(error => {
+            setIsSuccessful(False)
             setBlogs(`Could not fetch blog posts, error: ${error}`)
         })
     }, [])
 
-    if(isSuccessful) {
-        let blogsHTML = []
-        blogs.forEach((blog) => {
-            console.log(blog)
-            blogsHTML.push(
-                <div className="blog-post">
-                    <h2>{blog.title}</h2>
-                    <p>{blog.body}</p>
-                </div>
-            )
-        })
+    if (isSuccessful) {
         return (
             <>
                 <h1 className="header">Posts</h1>
-                {blogsHTML}
+                {blogs.map(blog => (
+                    <div className="blog-post" key={blog.id}>
+                        <h2>{blog.id}. {blog.title}</h2>
+                        <p>{blog.body}</p>
+                    </div>
+                ))}
             </>
         )
-    }else {
+    } else {
         return (
             <h1>{blogs}</h1>
         )
