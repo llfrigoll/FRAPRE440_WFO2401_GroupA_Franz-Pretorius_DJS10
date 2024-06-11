7f05ca1b646c0e79a424a91bf88ce5aa62273f25
Franz <franzj.pretorius.2003@gmail.com>
2024-06-11 14:34:55 +0200

    Fixed final changes and display

--- src/api.jsx
+++ src/api.jsx
@@ -2,22 +2,31 @@ import React from "react"
 import { useEffect, useState } from "react"
 
 function BlogComponent() {
+    //All states used during the project
     const [blogs, setBlogs] = useState([])
-    const [isSuccessful, setIsSuccessful] = useState(false);
+    const [isSuccessful, setIsSuccessful] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('')
 
+    //Using useEffect to fetch data
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {
-            setIsSuccessful(true)
-            setBlogs(data)
-        })
-        .catch(error => {
-            setIsSuccessful(False)
-            setBlogs(`Could not fetch blog posts, error: ${error}`)
-        })
+        const fetchBlogs = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+                if (!response.ok) {
+                    throw new Error('Could not fetch blogs')
+                }
+                const data = await response.json()
+                setIsSuccessful(true)
+                setBlogs(data)
+            } catch (error) {
+                setIsSuccessful(false)
+                setErrorMessage(error.message)
+            }
+        }
+        fetchBlogs()
     }, [])
 
+    //HTML renders pertaining on if the fetching of blogs was successful or not
     if (isSuccessful) {
         return (
             <>
@@ -32,7 +41,10 @@ function BlogComponent() {
         )
     } else {
         return (
-            <h1>{blogs}</h1>
+            <div className="error-message">
+                <h1>{errorMessage}</h1>
+            </div>
+            
         )
     }
 
